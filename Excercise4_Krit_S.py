805357c892a522ee03d47bb7a0c3a5097dc9a427
LeGoLus <finance001_@hotmail.com>
2019-07-04 20:41:12 +0700

    Score

---
+++ Excercise4_Krit_S.py
@@ -0,0 +1,10 @@
+English = 80
+GenBuess = 75.5
+IntroCom_Sys = 80
+ComPro = 88.5
+print("----------------Your Score---------------")
+print()
+print("Foundation English :",English,"Point")
+print("General Business:",GenBuess,"Point")
+print("Introduction to Computer Systems:",IntroCom_Sys,"Point")
+print("Computer Programming:",ComPro,"Point")
